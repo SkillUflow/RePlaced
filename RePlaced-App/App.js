34d389915a14758281a05844ae245d7abc6aac07
LouisDamay <112948472+LouisDamay@users.noreply.github.com>
2024-05-06 11:55:22 +0200

    APP_FRONT : Affichage de la carte en fonction des coordonnées
    - Actualisation toutes les 2 secondes

--- RePlaced-App/App.js
+++ RePlaced-App/App.js
@@ -1,26 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { getLocation } from './components/getLocation'
+import { StyleSheet, View } from 'react-native';
+import { getLocation } from './components/getLocation';
 
-let uCoords = []; 
+let userCoords = [0.65, 45.9167]; // Longitude et latitude par défaut
 
-export default function App() {
+const App = () => {
+
+  const [mapRegion, setMapRegion] = useState({
+    latitude: userCoords[1],
+    longitude: userCoords[0],
+    latitudeDelta: 0.001,
+    longitudeDelta: 0.001,
+  });
+
+  useEffect(() => {
+    const getUserLocation = async () => {
+      try {
+        const location = await getLocation();
+        const locationData = JSON.parse(location);
+
+        userCoords[0] = locationData.coords.longitude;
+        userCoords[1] = locationData.coords.latitude;
+
+        setMapRegion(prevRegion => ({
+          ...prevRegion,
+          latitude: userCoords[1],
+          longitude: userCoords[0],
+        }));
+
+        console.log("Position actuelle :");
+        console.log("\t LONGITUDE :", userCoords[0]);
+        console.log("\t  LATITUDE : ", userCoords[1]);
+        console.log("\n");
+
+      } catch (error) {
+        console.error(error);
+        alert("Une erreur est survenue lors de la récupération de la position. Veuillez relancer l'application RePlaced ;)");
+      }
+    };
+
+    // Actualiser la position toutes les 2 secondes
+    const intervalId = setInterval(getUserLocation, 2000);
+    return () => clearInterval(intervalId);
+
+  }, []);
 
   return (
-  
     <View style={styles.container}>
-      <Text>Il n'y a plus qu'à</Text>
-
       <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        region={mapRegion}
+        onRegionChangeComplete={region => setMapRegion(region)}
+      >
+        <Marker
+          coordinate={{ latitude: userCoords[1], longitude: userCoords[0] }}
+          title="Votre position"
+          description="Vous êtes ici"
+        />
+      </MapView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
   },
 });
+
+export default App;
