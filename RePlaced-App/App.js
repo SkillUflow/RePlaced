c95a17e0ba881947042a516d10cb8bfb0a51463e
Aymeric-Droulers <112798634+Aymeric-Droulers@users.noreply.github.com>
2024-05-06 16:58:02 +0200

    Ouverture de la modale au clic sur un marker

--- RePlaced-App/App.js
+++ RePlaced-App/App.js
@@ -3,17 +3,26 @@ import MapView, { Marker } from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Pressable, Image } from 'react-native';
 import { getLocation } from './components/getLocation';
+import Modale from './components/modale';
 
 let userCoords = [0.65, 45.9167]; // Longitude et latitude par défaut
 
 const App = () => {
 
+  const [modalVisible, setModalVisible] = useState(false);
+  var [CoordinateMarker,setCoordinateMarker]=useState({lat:0.65,long:45.9167})
   const [mapRegion, setMapRegion] = useState({
     latitude: userCoords[1],
     longitude: userCoords[0],
     latitudeDelta: 0.001,
     longitudeDelta: 0.001,
   });
+
+  const openModal = (coordinate)=>{
+    setCoordinateMarker(coordinate)
+    setModalVisible(!modalVisible);
+    StatusBar.hidden=true;
+  }
 
   useEffect(() => {
     const getUserLocation = async () => {
@@ -37,15 +46,16 @@ const App = () => {
     };
 
     // Actualiser la position toutes les 2 secondes
-    const intervalId = setInterval(getUserLocation, 2000);
-    return () => clearInterval(intervalId);
+    // const intervalId = setInterval(getUserLocation, 2000);
+    // return () => clearInterval(intervalId);
+    getUserLocation();
 
   }, []);
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
 
+      
       <MapView
         style={styles.map}
         region={mapRegion}
@@ -56,12 +66,25 @@ const App = () => {
           title="Votre position"
           description="Vous êtes ici"
         />
+        <Marker
+            key={3}
+            coordinate={{ latitude:50.629850 , longitude:3.066374  }}
+            onPress={() => openModal({ lat:50.629850 , long:3.066374  })}
+          />
+
+        <Marker
+            key={4}
+            coordinate={{ latitude:0 , longitude:0  }}
+            onPress={() => openModal({ lat:0 , long:0  })}
+          />
       </MapView>
     
       <Pressable onPress={() => console.log("Centrons la carte à présent :)")} style={styles.center_btn}>
         <Image source={require("./assets/buttons/center_map.png")} style={styles.center_btn_img}/>
       </Pressable>
 
+      <Modale modalVisible={modalVisible} setModalVisible={setModalVisible} coordonnes={CoordinateMarker}></Modale>
+      <StatusBar hidden={modalVisible} />
     </View>
   );
 };
---
+++ RePlaced-App/components/modale.js
@@ -0,0 +1,118 @@
+import React,{useState} from "react";
+import { View, Modal, Text, Pressable, Alert, StyleSheet, Linking,StatusBar} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const Modale = ({ modalVisible, setModalVisible,coordonnes}) => {
+console.log(coordonnes)
+let lat = coordonnes.lat
+let long = coordonnes.long
+
+const closeModal = () => {
+    setModalVisible(false);
+    StatusBar.hidden=false;
+};
+
+
+const openGoogleMaps = () => {
+  const latitude =50.629850; // Latitude de la destination
+  const longitude = 3.066374; // Longitude de la destination
+  const label = "Target Location"; // Label de la destination
+
+  // Format de l'URL pour l'itinéraire Google Maps
+  const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}&travelmode=driving&dir_action=navigate&destination_place_id=${label}`;
+
+  // Ouvrir l'URL dans Google Maps
+  Linking.openURL(url).catch((err) => console.error('An error occurred', err));
+};
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        Alert.alert('Modal has been closed.');
+        setModalVisible(!modalVisible);
+      }}>
+      <Pressable style={styles.centeredView} onPress={()=>closeModal()}>
+        <View style={styles.modalView} onStartShouldSetResponder={() => true}>
+            <Text style={styles.title}>3 places libres</Text>
+            <Text style={styles.text}>1 rue Watto</Text>
+            <Text>{coordonnes.lat},{coordonnes.long}</Text>
+            <View style={styles.btnBox}>
+              <Pressable onPress={() =>openGoogleMaps()} style={styles.btnPrimary}>
+                <Text  style={[styles.whiteText,styles.btnCenterText]}>Y aller</Text>
+              </Pressable>
+
+              <Pressable style={styles.btnSecondary}>
+                <Text style={[styles.btnCenterText]}>Je vais me garer ici</Text>
+              </Pressable>
+            </View>
+        </View>
+      </Pressable>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Transparence ajoutée
+    
+  },
+  modalView: {
+    flex:0.3,
+    width:"100%",
+    marginTop:0,
+    backgroundColor: 'white',
+    borderBottomLeftRadius:52,
+    borderBottomRightRadius:52,
+    padding: 35,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  btnCenterText:{
+    textAlign:"center",
+    fontSize:30
+  },
+
+  title:{
+    fontSize:30,
+    fontWeight:"bold"
+  },
+  text:{
+    fontSize:30,
+    fontWeight:"300"
+  },
+
+  btnBox:{
+    flex:1,
+    justifyContent:"space-evenly"
+  },
+  btnPrimary:{
+    backgroundColor:"#1C62CA",
+    borderRadius:8
+  },
+  whiteText:{
+    color:"white",
+  },
+
+  btnSecondary:{
+    borderColor:"#1C62CA",
+    backgroundColor:"transparent",
+    borderWidth:1,
+    borderRadius: 8
+  }
+
+  
+});
+
+export default Modale;
