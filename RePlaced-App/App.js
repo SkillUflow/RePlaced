21a8785646c24ba8806868b69565652fbd1bf4e8
Aymeric-Droulers <112798634+Aymeric-Droulers@users.noreply.github.com>
2024-05-08 11:23:52 +0200

    quelques tests de build

--- RePlaced-App/App.js
+++ RePlaced-App/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React,{useState} from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
