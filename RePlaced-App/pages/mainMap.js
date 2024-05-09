100d1099dd3a629107189e6af9ad16f38cbedf3b
Arsial <53660084+ToToChti@users.noreply.github.com>
2024-05-09 15:34:50 +0200

    Updated mainMap

--- RePlaced-App/pages/mainMap.js
+++ RePlaced-App/pages/mainMap.js
@@ -30,12 +30,7 @@ const MainMap = ({navigation, route}) => {
           }
         });
 
-        console.log("Réponse", response);
-
-
-        const resultat = await response.json();
-        console.log("Résultat", resultat);
-        
+        const resultat = await response.json();        
 
         setPinList(resultat);
 
