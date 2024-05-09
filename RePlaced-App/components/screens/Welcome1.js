1617ab14934150e508e8af59426a876c9f484ce1
Arsial <53660084+ToToChti@users.noreply.github.com>
2024-05-09 11:45:44 +0200

    Removed old unuseful windows

--- RePlaced-App/components/screens/Welcome1.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, View, Pressable, Image, Text} from 'react-native';
-
-const Welcome1 = () =>{
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Ecran d'accueil 1</Text>
-        </View>
-    );
-}
--- RePlaced-App/components/screens/Welcome3.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, View, Pressable, Image, Text} from 'react-native';
-
-const Welcome3 = () =>{
-    return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Ecran d'accueil 3</Text>
-        </View>
-    );
-}
