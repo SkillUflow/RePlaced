07546bbae86e544bcd0e09de82435aa302e289f6
Aymeric-Droulers <112798634+Aymeric-Droulers@users.noreply.github.com>
2024-05-13 14:01:43 +0200

    Onboarding amélioré

--- RePlaced-App/pages/welcome1.js
+++ RePlaced-App/pages/welcome1.js
@@ -1,84 +1,25 @@
-import React from 'react';
-import { View,Text,Button,StyleSheet,Pressable,Image, Modal } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import React, { useEffect } from 'react';
+import { View,Text,StyleSheet,Image} from 'react-native';
 import { useGlobalContext } from '../components/GlobalContext';
 
-import closeImg from "../assets/buttons/close.png"
-import lineLogo from "../assets/lineLogo.png"
+
 import image from "../assets/onboarding/imageOne.png"
 
 
-const Welcome1 = ({navigation, route}) =>{
+const Welcome1 = () =>{
 
-  const { sessionKey, setSessionKey, setConnModalVisible, serverURL } = useGlobalContext();
-
-  async function tryLogin() {
-
-    if(!sessionKey) {
-      navigation.navigate("MainMap");
-      setConnModalVisible(true);
-    }
-
-    else {
-
-      const response = await fetch(serverURL + "/isLogged", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          sessionKey
-        })
-      });
-
-      const resultat = await response.json();
-      
-      // Is user is logged in, immediately go to map
-      if(resultat.response) {
-        navigation.navigate("MainMap");
-        setConnModalVisible(false);
-      }
-      // Else, open login menu
-      else {
-        navigation.navigate("MainMap");
-        setConnModalVisible(true);
-      }
-    }
-  }
+    
+  useEffect(()=>{
+    console.log("1");
+  })
   
     return(
 
-        <View style={{flex:1}}>
-          <LinearGradient
-            colors={['#1C62CA', '#174D9E']}
-            style={styles.container}
-          >
-              <Pressable style={styles.closeBtn}  onPress={() => {navigation.navigate("MainMap")}}>
-                <Image source={closeImg} style={styles.closeImg}/>
-              </Pressable>
-              <Image source={lineLogo} style={styles.LogoImage}/>
-              <Image source={image} style={styles.mainImage}/>
-              <Text style={[styles.text,styles.centerText]}>Ne vous souciez plus de trouver une place</Text>
-              <Pressable style={styles.CTA} onPress={tryLogin}>
-                <Text style={styles.text}>Me connecter</Text>
-              </Pressable>
-
-              <View style={styles.circleContainer}>
-                <Pressable style={styles.bigBtnContainer} onPress={() => navigation.navigate('Welcome1')}>
-                  <Image style={styles.circle} source={require('../assets/buttons/circle.png')}></Image>
-                </Pressable>
-                <Pressable style={styles.smallBtnContainer} onPress={() => navigation.navigate('Welcome2')}>
-                  <Image style={styles.circle} source={require('../assets/buttons/circle.png')}></Image>
-                </Pressable>
-                <Pressable style={styles.smallBtnContainer} onPress={() => navigation.navigate('Welcome3')}>
-                  <Image style={styles.circle} source={require('../assets/buttons/circle.png')}></Image>
-                </Pressable>
-              </View>
-            
-             
+        <View style={styles.container}>
               
-                
-          </LinearGradient>
+          <Image source={image} style={styles.mainImage}/>
+          <Text style={[styles.text,styles.centerText]}>Ne vous souciez plus de trouver une place</Text>
+
         </View>
     );
 }
@@ -86,25 +27,14 @@ const Welcome1 = ({navigation, route}) =>{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 60,
     justifyContent: "space-between",
     alignItems: "center",
-    width: "100%"
+    backgroundColor:"#1C62CA",
+    paddingVertical:32
   },
-  
-  closeBtn: {
-    position: "absolute",
-    right: 20,
-    top: 50,
-    height: 30,
-    width: 30,
-  },
-  closeImg: {
-    height: "100%",
-    width: "100%",
-  },
+
   mainImage: {
-    height: "30%",
+    height: "70%",
     resizeMode: "contain",
   },
   text: {
@@ -116,44 +46,11 @@ const styles = StyleSheet.create({
   centerText: {
     width: "80%"
   },
-  CTA: {
-    borderWidth: 1,
-    borderColor: "white",
-    borderRadius: 16,
-    paddingHorizontal: 32,
-    paddingVertical: 8
-  },
 
-  LogoImage: {
-    width: '80%',
-    resizeMode: 'contain'
-  },
   
 
-  circleContainer: {
-    flexDirection: 'row',
-    width: '30%',
-    alignItems: 'center',
-    height: 80,
-    marginBottom: 30,
-    justifyContent: 'space-between'
-  },
+ 
 
-  smallBtnContainer: {
-    width: '20%',
-    height: '20%',
-  },
-
-  bigBtnContainer: {
-    width: '35%',
-    height: '35%',
-  },
-
-  circle: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'contain'
-  }
 });
   
 export default Welcome1;
