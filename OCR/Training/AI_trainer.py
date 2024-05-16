e4800760fda9314606fab929ec81355cf34c4f76
Romain <112857846+SkillUflow@users.noreply.github.com>
2024-05-16 22:10:04 +0200

    Created the AI trainer

--- OCR/Training/AI_trainer.py
+++ OCR/Training/AI_trainer.py
@@ -5,15 +5,54 @@ from core_functions import *
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
+size = (64, 64)
+
+def preprocess_image(image):
+    image = np.float32(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Resize the image
+    resized = cv2.resize(gray, size)
+
+    # Apply Gaussian blur
+    blur = cv2.GaussianBlur(resized, (5, 5), 0)
+
+    # Normalize the image (to scale pixel values between 0 and 1)
+    normalized = blur / 255.0
+
+    return normalized
+
 def load_data_for_training():
     # Load the data from the SQLite database
-    images, boxes, labels = _load_data_for_training(database_full_path)
+    images, boxes = _load_data_for_training(database_full_path)
+
+    # Preprocess the images
+    images = [preprocess_image(img) for img in images]
+
+    # Expand dimensions of the images, necessary for the model
+    images = np.expand_dims(images, axis=-1)
+
     # Split the dataset into a training set and a test set
-    images_train, images_test, boxes_train, boxes_test, labels_train, labels_test = train_test_split(images, boxes, labels, test_size=0.2, random_state=42)
+    images_train, images_test, boxes_train, boxes_test = train_test_split(images, boxes, test_size=0.2, random_state=42)
 
     # Now you can create separate TensorFlow Datasets for training and testing
-    train_dataset = tf.data.Dataset.from_tensor_slices((images_train, boxes_train, labels_train))
-    test_dataset = tf.data.Dataset.from_tensor_slices((images_test, boxes_test, labels_test))
+    train_dataset = tf.data.Dataset.from_tensor_slices((images_train, boxes_train))
+
+    # One-hot encode the labels
+    train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, depth=2)))
+
+    # Batch the data
+    batch_size = 32
+    train_dataset = train_dataset.batch(batch_size)
+
+    test_dataset = tf.data.Dataset.from_tensor_slices((images_test, boxes_test))
+
+    # One-hot encode the labels
+    test_dataset = test_dataset.map(lambda x, y: (x, tf.one_hot(y, depth=2)))
+
+    # Batch the data
+    test_dataset = test_dataset.batch(batch_size)
+
     return train_dataset, test_dataset
 
 def _load_data_for_training(db_file):
@@ -25,25 +64,26 @@ def _load_data_for_training(db_file):
     c.execute("""
         SELECT 
             images_area.image_path, 
-            parking_space.space_coordinates, 
-            parking_occupation_data.car_presence 
+            parking_space.space_coordinates
         FROM 
             parking_occupation_data 
         JOIN 
             images_area ON parking_occupation_data.image_id = images_area.image_id 
         JOIN 
             parking_space ON parking_occupation_data.coordinate_id = parking_space.space_coordinates_id
+        WHERE
+              parking_occupation_data.car_presence = 1
     """)
 
     # Lists to hold the images, bounding boxes, and labels
     images = []
     boxes = []
-    labels = []
-
     # Iterate over the query results
     for row in c.fetchall():
         # Load the image and normalize it to [0,1]
-        img = Image.open(row[0])
+        image_path = row[0]
+        #image_path = image_path.replace("originals", "processed")
+        img = Image.open(image_path)
         img = np.array(img) / 255.0
         images.append(img)
 
@@ -51,13 +91,10 @@ def _load_data_for_training(db_file):
         coordinates = tuple(map(int, row[1].split(',')))
         boxes.append([(coordinates[0], coordinates[1]), (coordinates[2], coordinates[3])])
 
-        # Append the label
-        labels.append(row[2])
-
     # Close the connection to the database
     conn.close()
 
-    return images, boxes, labels
+    return images, boxes
 
 
 def train_model(train_dataset):
@@ -65,21 +102,29 @@ def train_model(train_dataset):
 
     # Define the model
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(1, activation='sigmoid')
-    ])
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding="same", input_shape=(*size, 1)),  # Grayscale images have 1 channel
+    tf.keras.layers.MaxPooling2D((1, 1)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding="same"),
+    tf.keras.layers.MaxPooling2D((1, 1)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding="same"),
+    tf.keras.layers.MaxPooling2D((1, 1)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding="same"),
+    tf.keras.layers.MaxPooling2D((1, 1)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding="same"),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(8, activation='sigmoid'),  # Adjusted number of units
+    tf.keras.layers.Reshape((2, 2, 2))  # Reshape the output to match target shape
+])
 
     # Compile the model
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # Train the model
-    model.fit(train_dataset, epochs=10)
+    inputs, targets = next(iter(train_dataset))
+    print('Inputs shape:', inputs.shape)
+    print('Targets shape:', targets.shape)
+    model.fit(train_dataset, epochs=15)
 
     return model
 
@@ -96,5 +141,6 @@ print('Test accuracy:', test_acc)
 
 
 # Save the model
-model.save('parking_occupation_model.h5')
-print("Model saved as parking_occupation_model.h5")
+save_path = os.path.join(work_dir, 'parking_occupation_model.keras')
+model.save(save_path)
+print("Model saved as parking_occupation_model.keras")
