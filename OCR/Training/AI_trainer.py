ec8508e00ade9437562dda6371851ad4717bd134
Romain <romain.ryckebusch@student.junia.com>
2024-05-19 22:10:40 +0200

    Updated training and testing AIs. Most notably, created a generator to allow splitting the dataset in parts loaded on the run, thus not needing to store absolutely every image in memory

--- OCR/Training/AI_trainer.py
+++ OCR/Training/AI_trainer.py
@@ -5,109 +5,120 @@ from core_functions import *
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import datetime
+import math
 
 size = (64, 64)
-total_samples = 0
+batch_size = 32
+
 
 def preprocess_image(image):
-    image = np.float32(image)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Convert the image to float32 using TensorFlow operation
+    image = tf.cast(image, tf.float32)
+
+    # Convert the image to grayscale
+    gray = tf.image.rgb_to_grayscale(image)
 
     # Resize the image
-    resized = cv2.resize(gray, size)
+    resized = tf.image.resize(gray, size)
 
-    # Apply Gaussian blur
-    #blur = cv2.GaussianBlur(resized, (5, 5), 0)
-    blur = resized
-
-    # Normalize the image (to scale pixel values between 0 and 1)
-    normalized = blur / 255.0
+    # Normalize the image
+    normalized = resized / 255.0
 
     return normalized
 
 def load_data_for_training():
-    # Load the data from the SQLite database
-    images, boxes = _load_data_for_training(database_full_path)
-
-    # Preprocess the images
-    images = [preprocess_image(img) for img in images]
-
-    # Expand dimensions of the images, necessary for the model
-    images = np.expand_dims(images, axis=-1)
-
-    # Split the dataset into a training set and a test set
-    images_train, images_test, boxes_train, boxes_test = train_test_split(images, boxes, test_size=0.2, random_state=42)
-
-    # Now you can create separate TensorFlow Datasets for training and testing
-    train_dataset = tf.data.Dataset.from_tensor_slices((images_train, boxes_train))
-
-    # One-hot encode the labels
-    train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, depth=1)))
-
-    # Batch the data
     batch_size = 32
-    train_dataset = train_dataset.batch(batch_size)
 
-    test_dataset = tf.data.Dataset.from_tensor_slices((images_test, boxes_test))
+    # Create a generator for the training data
+    train_data_generator = _load_data_for_training(database_full_path, batch_size, 'train')
 
-    # One-hot encode the labels
-    test_dataset = test_dataset.map(lambda x, y: (x, tf.one_hot(y, depth=1)))
+    # Create a TensorFlow Dataset from the generator
+    train_dataset = tf.data.Dataset.from_generator(
+        lambda: train_data_generator,
+        output_signature=(
+            tf.TensorSpec(shape=(None, 64, 64, 1), dtype=tf.float32),
+            tf.TensorSpec(shape=(None,), dtype=tf.int32)
+        )
+    )
 
-    # Batch the data
-    test_dataset = test_dataset.batch(batch_size)
+    # Preprocess the images and one-hot encode the labels
+    #train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, depth=1)))
+
+    # Create a generator for the test data
+    test_data_generator = _load_data_for_training(database_full_path, batch_size, 'test')
+
+    # Create a TensorFlow Dataset from the generator
+    test_dataset = tf.data.Dataset.from_generator(
+        lambda: test_data_generator,
+        output_signature=(
+            tf.TensorSpec(shape=(None, 64, 64, 1), dtype=tf.float32),
+            tf.TensorSpec(shape=(None,), dtype=tf.int32)
+        )
+    )
+
+    # Preprocess the images and one-hot encode the labels
+    #test_dataset = test_dataset.map(lambda x, y: (x, tf.one_hot(y, depth=1)))
 
     return train_dataset, test_dataset
 
-def _load_data_for_training(db_file):
-    batch_size = 32 # Define the batch size for training
 
-    # Connect to the SQLite database
-    conn = sqlite3.connect(db_file)
-    c = conn.cursor()
+def _load_data_for_training(db_file, batch_size=32, mode='train'):
+    while True:
+        # Calculate the number of rows for training and testing
+        c = sqlite3.connect(db_file).cursor()
+        # Query the database to get the total number of rows
+        c.execute("SELECT COUNT(*) FROM parking_occupation_data")
+        total_rows = c.fetchone()[0]
+        c.close()
+        train_rows = int(total_rows * 0.9)
+        test_rows = total_rows - train_rows
 
-    # Query the database to join the necessary tables
-    c.execute("""
-        SELECT 
-            images_area.image_path, 
-            parking_space.space_coordinates,
-            parking_occupation_data.car_presence
-        FROM 
-            parking_occupation_data 
-        JOIN 
-            images_area ON parking_occupation_data.image_id = images_area.image_id 
-        JOIN 
-            parking_space ON parking_occupation_data.coordinate_id = parking_space.space_coordinates_id
-    """)
-    results = c.fetchall()
-    total_samples = len(results) # Store the total number of samples for later use
-    # Lists to hold the images, bounding boxes, and labels
-    images = []
-    boxes = []
-    # Iterate over the query results
-    for i, row in enumerate(results):
-        # Load the image and normalize it to [0,1]
-        image_path = row[0]
-        img = Image.open(image_path)
-        img = np.array(img) / 255.0
-        images.append(img)
+        # Determine the offset based on the mode
+        offset = 0 if mode == 'train' else train_rows
 
-        # Parse the coordinates string into a tuple of integers
-        coordinates = tuple(map(int, row[1].split(',')))
-        car_presence = row[2]
-        boxes.append(car_presence)
+        # Determine the limit based on the mode
+        limit = train_rows if mode == 'train' else test_rows
 
-        # If the batch size is reached, yield the batch and reset the lists
-        if (i + 1) % batch_size == 0:
-            yield np.array(images), np.array(boxes)
+        for start in range(offset, offset + limit, batch_size):
+            # Connect to the SQLite database
+            conn = sqlite3.connect(db_file)
+            c = conn.cursor()
+
+            # Query the database to join the necessary tables
+            c.execute("""
+                SELECT 
+                    images_area.image_path, 
+                    parking_space.space_coordinates,
+                    parking_occupation_data.car_presence
+                FROM 
+                    parking_occupation_data 
+                JOIN 
+                    images_area ON parking_occupation_data.image_id = images_area.image_id 
+                JOIN 
+                    parking_space ON parking_occupation_data.coordinate_id = parking_space.space_coordinates_id
+                LIMIT ? OFFSET ?
+            """, (batch_size, start))
+
+            results = c.fetchall()
+
             images = []
             boxes = []
-        
 
-    # Close the connection to the database
-    conn.close()
+            for row in results:
+                # Load the image and normalize it to [0,1]
+                image_path = row[0]
+                img = Image.open(image_path)
+                images.append(preprocess_image(img))
 
-    return images, boxes
+                # Parse the coordinates string into a tuple of integers
+                coordinates = tuple(map(int, row[1].split(',')))
+                car_presence = row[2]
+                boxes.append(car_presence)
 
+            # Close the connection to the database
+            conn.close()
+
+            yield images, boxes
 
 def train_model(train_dataset):
     # Load the training data
@@ -129,18 +140,28 @@ def train_model(train_dataset):
 ])
 
     # Compile the model
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'], learning_rate=0.0005)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    # Train the model
-    inputs, targets = next(iter(train_dataset))
-    print('Inputs shape:', inputs.shape)
-    print('Targets shape:', targets.shape)
-    model.fit(train_dataset, epochs=10, steps_per_epoch=total_samples // batch_size)
+    # Calculate the number of steps per epoch
+    c = sqlite3.connect(database_full_path).cursor()
+    total_rows = c.execute("SELECT COUNT(*) FROM parking_occupation_data").fetchone()[0]
+    c.close()
+    epochs_number = 10
+    train_rows = int(total_rows * 0.9)
+    steps_per_epoch = train_rows // batch_size
+    print('Steps per epoch:', steps_per_epoch)
+    print("total_rows: ", total_rows)
+
+    model.fit(train_dataset, epochs=epochs_number, steps_per_epoch=steps_per_epoch)
     return model
 
 if __name__ == "__main__":
     # Load the training data
     train_dataset, test_dataset = load_data_for_training()
+
+    # Repeat and shuffle the training data
+    #train_dataset = train_dataset.shuffle(buffer_size=1024) READD AFTERWARDS, REMOVED ONLY FOR TESTING
+    train_dataset = train_dataset.repeat()
 
     # Train the model
     model = train_model(train_dataset)
@@ -154,6 +175,8 @@ if __name__ == "__main__":
     # Save the model
     now_str = datetime.datetime.now().isoformat().replace(":", "")
 
+    if not os.path.exists('OCR/Training/models'):
+        os.makedirs('OCR/Training/models') # Ensure the directory exists (it tends to get deleted by git since the keras files aren't pushed)
 
     save_path =  'OCR/Training/models/parking_occupation_model_' + now_str + '.keras'
     model.save(save_path)
