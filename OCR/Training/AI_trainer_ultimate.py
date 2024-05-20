1ea4c1dc35d18c5e284705466a912308af0207f8
Romain <romain.ryckebusch@student.junia.com>
2024-05-20 05:11:07 +0200

    Made many updates to trainer, and created alternate versions depending on the computer's processing power

--- OCR/Training/AI_trainer_ultimate.py
+++ OCR/Training/AI_trainer_ultimate.py
@@ -9,8 +9,9 @@ import datetime
 import math
 
 size = (64, 64)
-batch_size = 32
-database_full_path = 'OCR/Training/trimmed_training_data.db'
+batch_size = 512
+database_old_path = 'OCR/Training/training_data.db'
+database_new_path = 'OCR/Training/trimmed_training_data.db'
 metrics = [FBetaScore(threshold=0.5, beta=4.0), 'accuracy', 'recall', 'precision', 'AUC']
 
 
@@ -34,11 +35,11 @@ def preprocess_image(image):
 
     return blurred
 
-def load_data_for_training():
+def load_data_for_training(database_path):
     batch_size = 32
 
     # Create a generator for the training data
-    train_data_generator = _load_data_for_training(database_full_path, batch_size, 'train')
+    train_data_generator = _load_data_for_training(database_path, batch_size, 'train')
 
     # Create a TensorFlow Dataset from the generator
     train_dataset = tf.data.Dataset.from_generator(
@@ -53,7 +54,7 @@ def load_data_for_training():
     #train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, depth=1)))
 
     # Create a generator for the test data
-    test_data_generator = _load_data_for_training(database_full_path, batch_size, 'test')
+    test_data_generator = _load_data_for_training(database_path, batch_size, 'test')
 
     # Create a TensorFlow Dataset from the generator
     test_dataset = tf.data.Dataset.from_generator(
@@ -129,7 +130,7 @@ def _load_data_for_training(db_file, batch_size=32, mode='train'):
 
             yield images, boxes
 
-def train_model(train_dataset, metric):
+def train_model(train_dataset, metric, database_full_path):
     # Load the training data
 
     # Define the model
@@ -151,10 +152,10 @@ def train_model(train_dataset, metric):
     # Compile the model
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[metric])
     # Calculate the number of steps per epoch
-    c = sqlite3.connect(database_full_path).cursor()
+    c = sqlite3.connect(database_path).cursor()
     total_rows = c.execute("SELECT COUNT(*) FROM parking_occupation_data").fetchone()[0]
     c.close()
-    epochs_number = 10
+    epochs_number = 15
     train_rows = int(total_rows * 0.9)
     steps_per_epoch = train_rows // batch_size
     print('Steps per epoch:', steps_per_epoch)
@@ -169,30 +170,31 @@ def train_model(train_dataset, metric):
 
 if __name__ == "__main__":
     for metric in metrics:
-        # Load the training data
-        train_dataset, test_dataset = load_data_for_training()
+        for database_path in [database_old_path, database_new_path]:
+            # Load the training data
+            train_dataset, test_dataset = load_data_for_training(database_path)
 
-        # Repeat and shuffle the training data
-        train_dataset = train_dataset.shuffle(buffer_size=1024)
-        train_dataset = train_dataset.repeat()
+            # Repeat and shuffle the training data
+            #train_dataset = train_dataset.shuffle(buffer_size=1024)
+            train_dataset = train_dataset.repeat()
 
-        # Train the model
-        model = train_model(train_dataset, metric)
+            # Train the model
+            model = train_model(train_dataset, metric, database_path)
 
-        # Save the model
-        now_str = datetime.datetime.now().isoformat().replace(":", "")
+            # Save the model
+            now_str = datetime.datetime.now().isoformat().replace(":", "")
 
-        if not os.path.exists('OCR/Training/models'):
-            os.makedirs('OCR/Training/models') # Ensure the directory exists (it tends to get deleted by git since the keras files aren't pushed)
+            if not os.path.exists('OCR/Training/models'):
+                os.makedirs('OCR/Training/models') # Ensure the directory exists (it tends to get deleted by git since the keras files aren't pushed)
 
-        save_path =  'OCR/Training/models/parking_occupation_model_' + now_str + '.keras'
-        model.save(save_path)
-        print("Model saved as ", save_path)
+            save_path =  'OCR/Training/models/parking_occupation_model_' + now_str + '.keras'
+            model.save(save_path)
+            print("Model saved as ", save_path)
 
-        # test the model
-        c = sqlite3.connect(database_full_path).cursor()
-        total_rows = c.execute("SELECT COUNT(*) FROM parking_occupation_data").fetchone()[0]
-        c.close()
-        test_loss, test_acc = model.evaluate(test_dataset, steps=math.ceil(0.1 * total_rows / batch_size))
-        print('Test accuracy:', test_acc)
-        print('Test loss:', test_loss)
+            # test the model
+            c = sqlite3.connect(database_path).cursor()
+            total_rows = c.execute("SELECT COUNT(*) FROM parking_occupation_data").fetchone()[0]
+            c.close()
+            test_loss, test_acc = model.evaluate(test_dataset, steps=math.ceil(0.1 * total_rows / batch_size))
+            print('Test accuracy:', test_acc)
+            print('Test loss:', test_loss)
