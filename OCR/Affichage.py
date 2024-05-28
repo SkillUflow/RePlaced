464e001613c80cfcd59d8781554c9b2f3ea7c20c
KhalidBensaber <khalid.bensaber@student.junia.com>
2024-05-28 04:21:52 +0200

    OK

--- OCR/Affichage.py
+++ OCR/Affichage.py
@@ -51,8 +51,6 @@ def box_label(image, box, label='',color=(128, 128, 128), txt_color=(255, 255, 2
                 lineType=cv2.LINE_AA)
 
 def findObjects(outputs, img, score=True):
-    xSIZE = 45
-    ySIZE = 45  
     boxes = outputs[0].boxes.data
         
     labels = {0: u'__background__', 1: u'person', 2: u'bicycle',3: u'car', 4: u'motorcycle', 5: u'airplane', 6: u'bus', 7: u'train', 8: u'truck', 9: u'boat', 10: u'traffic light', 11: u'fire hydrant', 12: u'stop sign', 13: u'parking meter', 14: u'bench', 15: u'bird', 16: u'cat', 17: u'dog', 18: u'horse', 19: u'sheep', 20: u'cow', 21: u'elephant', 22: u'bear', 23: u'zebra', 24: u'giraffe', 25: u'backpack', 26: u'umbrella', 27: u'handbag', 28: u'tie', 29: u'suitcase', 30: u'frisbee', 31: u'skis', 32: u'snowboard', 33: u'sports ball', 34: u'kite', 35: u'baseball bat', 36: u'baseball glove', 37: u'skateboard', 38: u'surfboard', 39: u'tennis racket', 40: u'bottle', 41: u'wine glass', 42: u'cup', 43: u'fork', 44: u'knife', 45: u'spoon', 46: u'bowl', 47: u'banana', 48: u'apple', 49: u'sandwich', 50: u'orange', 51: u'broccoli', 52: u'carrot', 53: u'hot dog', 54: u'pizza', 55: u'donut', 56: u'cake', 57: u'chair', 58: u'couch', 59: u'potted plant', 60: u'bed', 61: u'dining table', 62: u'toilet', 63: u'tv', 64: u'laptop', 65: u'mouse', 66: u'remote', 67: u'keyboard', 68: u'cell phone', 69: u'microwave', 70: u'oven', 71: u'toaster', 72: u'sink', 73: u'refrigerator', 74: u'book', 75: u'clock', 76: u'vase', 77: u'scissors', 78: u'teddy bear', 79: u'hair drier', 80: u'toothbrush'}
@@ -94,20 +92,7 @@ async def get_frame_async(cam, screens, Error):
         Error[cam.name] = str(e)
         return None
 
-async def process_camera(cam, screens, Error):
-    while True:
-        img = await get_frame_async(cam, screens, Error)
-        resized_up = Frame_Process(img) if img is not None else None
-        outputs = model.predict(img) if img is not None else None
-        cnt = findObjects(outputs, resized_up) if outputs and resized_up is not None else 0
-        
-        cv2.imshow('Image', resized_up) #Display images with the cars detected
 
-        if cv2.waitKey(22) & 0xFF == ord('q') or (cv2.getWindowProperty('Image', cv2.WND_PROP_VISIBLE) < 1):
-            cv2.destroyAllWindows()
-            break
-        
-    return cam, cnt
 
 async def main():
     global confThreshold
@@ -135,7 +120,11 @@ async def main():
         
 
     Allcams = [cam for cam in cams if cam.name not in str(blacklist)]
-    cams = [Allcams[0], Allcams[1]]
+    cams = []
+    names = ["","878868 ","812272","804453"]
+    for cam in Allcams:
+        if cam.name in names:
+            cams.append(cam)
     """
     cams = []
     BlackCams.filter(lambda cam: cam.name not in str(blacklist))
@@ -148,11 +137,36 @@ async def main():
 
     Error = {}
     
+
+    """
     tasks = [process_camera(cam, screens, Error) for cam in cams]
     for task in asyncio.as_completed(tasks):
         cam, cnt, NbPlaces = await task
-        result[cam.name] = f"{cnt}/{NbPlaces}"
-        print(f"Camera {cam.name} has {cnt} cars for {NbPlaces} places available\n")
+    """
+    
+    
+    while True:
+        img = await get_frame_async(cams[0], screens, Error)
+        outputs = model.predict(img) if img is not None else None
+        cnt = findObjects(outputs, img) if outputs and img is not None else 0
+        resized_up = Frame_Process(img) if img is not None else None
+        
+        
+        
+        
+        
+        img2 = await get_frame_async(cams[1], screens, Error)
+        outputs2 = model.predict(img2) if img is not None else None
+        cnt2 = findObjects(outputs2, img2) if outputs2 and img2 is not None else 0
+        resized_up2 = Frame_Process(img2) if img2 is not None else None
+        
+        
+        cv2.imshow('Image', resized_up) #Display images with the cars detected
+        cv2.imshow('Image2', resized_up2)
+
+        if cv2.waitKey(22) & 0xFF == ord('q') or (cv2.getWindowProperty('Image', cv2.WND_PROP_VISIBLE) < 1):
+            cv2.destroyAllWindows()
+            break
     
 
     if Error:
